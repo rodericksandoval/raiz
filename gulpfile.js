9fbfbfaa8b72df11551192cb5c95be679d2b71fd
Roderick Sandoval <rodericksandoval@gmail.com>
2014-07-07 13:40:00 -0600

    cambios gulpile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,24 @@
 // Load plugins
-var gulp   = require('gulp'),
-    prefix = require('gulp-autoprefixer'),
-    notify = require('gulp-notify'),
-    sass   = require('gulp-ruby-sass'),
-    shell  = require('gulp-shell'),
-    uglify = require('gulp-uglify');
+//----------------------------------------------------------
+var gulp    = require('gulp'),
+    prefix  = require('gulp-autoprefixer'),
+    notify  = require('gulp-notify'),
+    sass    = require('gulp-ruby-sass'),
+    shell   = require('gulp-shell'),
+    svg2png = require('gulp-svg2png'),
+    svgmin  = require('gulp-svgmin'),
+    uglify  = require('gulp-uglify');
 
 
-// Sass task
+// Styles task
+//----------------------------------------------------------
 gulp.task('sass', function () {
   gulp.src('src/scss/*.scss')
   .pipe(sass({
     require: ['susy'],
     noCache: true,
-    // style: "compressed",
-    // compass: true,
+    style: "compressed",
+    compass: true,
     lineNumbers: true,
     loadPath: 'scss/*'
   }))
@@ -24,7 +28,23 @@ gulp.task('sass', function () {
 });
 
 
-// Uglify task
+// Images task
+//----------------------------------------------------------
+gulp.task('img', function () {
+  // Reducir svgs
+  gulp.src('src/img/*.svg')
+  .pipe(svgmin())
+  .pipe(gulp.dest('img'))
+  // Generar pngs
+  gulp.src('src/img/*.svg')
+  .pipe(svg2png())
+  .pipe(gulp.dest('img'))
+  .pipe(notify({ message: 'Images task complete' }));
+});
+
+
+// Scripts task
+//----------------------------------------------------------
 gulp.task('js', function () {
   gulp.src('src/js/**/*.js')
   .pipe(uglify())
@@ -34,22 +54,20 @@ gulp.task('js', function () {
 
 
 // Drush task
-gulp.task('drush', shell.task([
-  'drush cc all'
-]));
+//----------------------------------------------------------
 
-// gulp.task('drush', function () {
-//   .pipe(shell([
-//     'drush cc all'
-//     ]))
-//   .pipe(notify({ message: 'Drush task complete' }));
-// })
+gulp.task('drush', shell.task([
+  'drush cache-clear theme-registry'
+]));
 
 
 // Watch task
+//----------------------------------------------------------
 gulp.task('watch', function() {
   // Watch .scss files to run sass task.
   gulp.watch('src/scss/**/*.scss', ['sass']);
+  // Watch .svg files to run img task.
+  gulp.watch('src/img/*.svg', ['img']);
   // Watch .js files to run js task.
   gulp.watch('src/js/**/*.js', ['js']);
   // Watch php, inc and info file changes to run drush task.
@@ -58,5 +76,6 @@ gulp.task('watch', function() {
 
 
 // Default task
-gulp.task('default', ['sass', 'js', 'drush', 'watch']);
+//----------------------------------------------------------
+gulp.task('default', ['sass', 'img', 'js', 'drush', 'watch']);
 
